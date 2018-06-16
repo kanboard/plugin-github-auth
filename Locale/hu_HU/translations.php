bfd25dc2d8568bbe82e91346c6839a078f13c825
Balázs Úr <urbalazs@gmail.com>
2018-06-16 18:12:03 +0200

    Updated Hungarian translation

--- Locale/hu_HU/translations.php
+++ Locale/hu_HU/translations.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-    'Login with my Github Account' => 'Jelentkezzen be Github fiókkal',
-    'Link my Github Account' => 'Github fiók csatolása',
-    'Unlink my Github Account' => 'Github fiók leválasztása',
+    'Login with my Github Account' => 'Bejelentkezés a Github fiókommal',
+    'Link my Github Account' => 'Összekapcsolás a Github fiókommal',
+    'Unlink my Github Account' => 'A Github fiókom összekapcsolásának megszüntetése',
     'Github Account' => 'Github fiók',
-    // 'Github Authentication' => '',
-    // 'Help on Github authentication' => '',
+    'Github Authentication' => 'Github hitelesítés',
+    'Help on Github authentication' => 'Súgó a Github hitelesítéshez',
 );
 
